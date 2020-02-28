ab1d1f072fa25bd0988dc77007d86d7ecb10d858
vyevenko <vyeve@ik-tech.io>
2020-02-28 13:07:19 +0200

    added slick greeter

--- installation/060-install-core-software.sh
+++ installation/060-install-core-software.sh
@@ -4,12 +4,9 @@ set -e
 # source: https://dzone.com/articles/arch-linux-installation-on-hw-with-i3-windows-mana-1
 
 echo "################################################################"
-echo "Install Display Manager"   
+echo "Install Fonts"   
 echo "################################################################"
 
-sudo pacman -S lightdm lightdm-gtk-greeter --noconfirm --needed
-
-# fonts:
 sudo pacman -S noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont \
 ttf-liberation ttf-droid ttf-inconsolata ttf-roboto terminus-font ttf-font-awesome \
 --noconfirm --needed
@@ -34,4 +31,4 @@ echo "################################################################"
 sudo pacman -S firefox --noconfirm --needed
 sudo pacman -S nemo baobab net-tools wget transmission-gtk --noconfirm --needed
 
-yay -S urxvt-font-size-git sublime-text --noconfirm
+yay -S urxvt-font-size-git sublime-text --noconfirm --needed
---
+++ installation/070-install-lightdm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# source: https://dzone.com/articles/arch-linux-installation-on-hw-with-i3-windows-mana-1
+
+echo "################################################################"
+echo "Install Display Manager"   
+echo "################################################################"
+
+sudo pacman -S lightdm --noconfirm --needed
+
+yay -S --noconfirm --needed lightdm-slick-greeter
