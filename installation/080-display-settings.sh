76a4072d46c5cbd36277c6032462eb371efdbf74
vyevenko <vyeve@ik-tech.io>
2020-03-22 16:23:40 +0200

    renamed packages

--- installation/080-display-settings.sh
+++ installation/080-display-settings.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo cp settings2/lightdm/* /etc/lightdm/
 sudo sudo mkdir -p /usr/share/backgrounds/
-sudo cp settings2/background/* /usr/share/backgrounds/
+sudo cp settings2/backgrounds/* /usr/share/backgrounds/
 
 
 echo "################################################################"
@@ -11,7 +11,6 @@ echo "#########      root files copied                ################"
 echo "################################################################"
 
 yay -S --noconfirm qogir-gtk-theme-git
-yay -S --noconfirm zafiro-icon-theme
+yay -S --noconfirm breeze-show-cursor-theme
 sudo pacman -S --noconfirm papirus-icon-theme
 sudo pacman -S gtk-engine-murrine gtk-engines
-# git clone https://github.com/vinceliuice/Qogir-theme.git
